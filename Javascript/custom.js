b8e89c9a8adb6622ea89b94faba61f1c77314e2e
pkoriris <kipkoririsco4@gmail.com>
2024-03-06 19:31:21 +0300

    add javascript file

---
+++ Javascript/custom.js
@@ -0,0 +1,28 @@
+const FFEMALE_NAMES=[
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+
+];
+const MALE_NAMES=[
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame"
+];
+const DAYS_OF_THE_WEEK=[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
