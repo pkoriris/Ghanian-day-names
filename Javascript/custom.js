52e0d31e165480df823536b720f9c97182b9fe8d
pkoriris <kipkoririsco4@gmail.com>
2024-03-06 19:32:33 +0300

    add javascript file with compute function

--- Javascript/custom.js
+++ Javascript/custom.js
@@ -26,3 +26,18 @@ const DAYS_OF_THE_WEEK=[
     "Friday",
     "Saturday"
 ];
+
+// calculate day of the week as an interger btween 0-6
+function computeDayOfTheweek(){
+
+    let month = getMonth();
+    let day = getDay();
+    let year = getYear();
+    let century = parseInt (year.substring(0,2));
+    year=parseInt(year);
+
+    let dayOfTheWeek = Math.round (( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
+
+    return dayOfTheWeek;
+    
+}
